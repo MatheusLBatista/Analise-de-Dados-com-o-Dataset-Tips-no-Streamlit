5e13d869bcf743477b1e0358985d4c9bbd4f1b39
Matheus Lucas Batista <2024103070005@aluno.ifro.edu.br>
2026-02-19 19:38:33 -0400

    feat: testing streamlit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Esse é o titulo do meu primeiro projeto StreamLit")
+st.write("Esse é um texto qualquer.")
+
+st.map()
+
+st.button('Clique aqui')
+
+st.file_uploader('Faça o upload do arquivo aqui')
+
+st.text_input('Input de texto')
