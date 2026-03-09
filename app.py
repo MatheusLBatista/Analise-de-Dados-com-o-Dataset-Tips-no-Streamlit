9693b692ab528cd77368da1f3eee7e6f3f200638
Matheus Batista <matheusifro2020@gmail.com>
2026-03-09 13:34:42 -0400

    feat: creating regression plot between total_bill and tip

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
-
-st.title("Análise de Dados com o Dataset Tips no Streamlit")
+import matplotlib.pyplot as plt
+import numpy as np
 
 st.title("Atividade - Streamlit + Dataset Tips")
 
@@ -11,4 +11,17 @@ df = sns.load_dataset("tips")
 st.subheader("Prévia do DataSet")
 st.dataframe(df)
 
-st.subheader("Existe correlação entre o valor total da conta (total_bill) e o valor da gorjeta (tip)")
+st.subheader("1 - Existe correlação entre o valor total da conta (total_bill) e o valor da gorjeta (tip)?")
+
+fig, ax = plt.subplots()
+
+sns.regplot(
+    data = df, 
+    x = "total_bill",
+    y = "tip",
+    ax = ax
+)
+
+st.pyplot(fig)
+
+st.write("Sim, há uma correlação positiva entre o valor total da conta e o valor da gorjeta. À medida que o valor total da conta aumenta, o valor da gorjeta também tende a aumentar. Isso se torna visível no gráfico, onde os pontos apresentados formam uma tendência ascendente, evidenciando que os clientes tendem a dar gorjetas maiores quando a conta é mais cara.")
